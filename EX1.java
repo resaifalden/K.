fa2edd0bc4bc0b9a63556389bfa66b9c3d8e37cd
سيف الدين <resaifalden@gmail.com>
2026-03-16 20:51:09 +0300

    Add files via upload

---
+++ EX1.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.saifalden;
+
+import java.util.Scanner;
+
+
+public class EX1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner in = new Scanner (System.in);
+        
+        System.out.print("please enter the first nuumber :");
+        int i = in.nextInt();
+        System.out.print("enter the second number :");        
+        int x = in.nextInt();
+        int sum = i + x ;
+        System.out.println("sum is = "+ sum);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
